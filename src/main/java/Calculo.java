cca3f489a977f47aedde667e12f5ac10c30f86d7
John Douglas Taiwan de Sousa Costa <johnsousa61@gmail.com>
2019-04-20 12:41:01 -0300

    método calculoConsumoDia pronto. Alterações nas regras de negócio incluídas. Alterações sobre dias do mês e do ano

--- src/main/java/Calculo.java
+++ src/main/java/Calculo.java
@@ -2,6 +2,7 @@ public class Calculo {
     static final int MIL = 1000;
 
     private double kWHora = 0.0;
+    private double consumoPorDia = 0.0;
 
     Equipamento equipamento = new Equipamento();
 
@@ -12,6 +13,15 @@ public class Calculo {
         //transformando em KW//h
         this.kWHora = consumo / MIL;
         return this.kWHora;
+    }
+
+    public double calculoConsumoDia(double potencia, int hora, int quantidadeDia) {
+        //consumo em W/h.
+        double consumo = potencia * hora;
+
+        //consumo em Wh/dia
+        this.consumoPorDia = consumo * quantidadeDia;
+        return this.consumoPorDia;
     }
 
 
