4be20cd6efd55b8d3d5cda83ceec44d0899f43fb
John Douglas Taiwan de Sousa Costa <johnsousa61@gmail.com>
2019-04-21 14:39:11 -0300

    metodo que calcula o consumo por hora criado.

--- src/main/java/Calculo.java
+++ src/main/java/Calculo.java
@@ -1,28 +1,43 @@
 public class Calculo {
     static final int MIL = 1000;
+    static final int MES = 30;
 
     private double kWHora = 0.0;
     private double consumoPorDia = 0.0;
+    private double consumoPorMes = 0.0;
 
     Equipamento equipamento = new Equipamento();
 
-    public double calculoHora(double potencia, int hora) {
+    public double cconsumoPorHora(double potencia, int hora) {
         //consumo em W/h.
-        double consumo = potencia * hora;
+        double consumoWH = calculoConsumo(potencia,hora);
 
         //transformando em KW//h
-        this.kWHora = consumo / MIL;
+        this.kWHora = consumoWH / MIL;
         return this.kWHora;
     }
 
     public double calculoConsumoDia(double potencia, int hora, int quantidadeDia) {
         //consumo em W/h.
-        double consumo = potencia * hora;
+        double consumoWH = calculoConsumo(potencia,hora);
 
         //consumo em Wh/dia
-        this.consumoPorDia = consumo * quantidadeDia;
+        this.consumoPorDia = consumoWH * quantidadeDia;
         return this.consumoPorDia;
     }
 
+    public double calculoConsumoMes(double potencia, int hora, int quantidadeMes) {
+        //consumo em W/h.
+        double consumoWH = calculoConsumo(potencia,hora);
 
+        //consumo por mês em Wh/mês
+        this.consumoPorMes = consumoWH + (MES * quantidadeMes);
+        return this.consumoPorMes;
+    }
+
+    //método que calcula o consumo por hora e devolve em Wh
+    private double calculoConsumo(double pontencia, int hora) {
+        double consumo = pontencia * hora;
+        return consumo;
+    }
 }
