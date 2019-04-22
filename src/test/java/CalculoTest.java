63f3aa47f81d6851bee8133ffd9bed46c720713d
John Douglas Taiwan de Sousa Costa <johnsousa61@gmail.com>
2019-04-21 21:30:49 -0300

    iniciando o desenvolvimento de construtores

---
+++ src/test/java/CalculoTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculoTest {
+
+    @Test
+    void cconsumoPorHora() {
+    }
+
+    @Test
+    void calculoConsumoDia() {
+    }
+
+    @Test
+    void calculoConsumoMes() {
+    }
+
+    @Test
+    void calculoConsumoPorAno() {
+    }
+}
