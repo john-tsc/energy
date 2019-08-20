714d2d2301ef0cb39c0703c9861c98435bc81499
John Douglas Taiwan de Sousa Costa <johnsousa61@gmail.com>
2019-08-20 20:44:25 -0300

    Fazendo algumas alterações nas regras de negócio

--- src/main/java/Calculo.java
+++ src/main/java/Calculo.java
@@ -5,17 +5,18 @@ public class Calculo {
     private double kWHora = 0.0;
     private double consumoPorDia = 0.0;
     private double consumoPorMes = 0.0;
+    private double consumoEmReal = 0.0;
 
     Equipamento equipamento = new Equipamento();
 
     public Calculo() {
     }
 
-    public double cconsumoPorHora(double potencia, int hora) {
+    public double consumoPorHora(double potencia, int hora) {
         //consumo em W/h.
         double consumoWH = calculoConsumo(potencia, hora);
 
-        //transformando em KW//h
+        //transformando em KW/h
         this.kWHora = consumoWH / MIL;
         return this.kWHora;
     }
@@ -36,6 +37,14 @@ public class Calculo {
         //consumo por mês em Wh/mês
         this.consumoPorMes = consumoWH + (MES * quantidadeMes);
         return this.consumoPorMes;
+    }
+
+    //método para calcular o consumo em Real (R$)
+    private double calculoConsumoReal(){ /*
+    se fizer desta forma então será necessário o consumo em Kwh. Por este motivo irá precisar que use o método
+    correspondente antes de usar este método. Ou fazer o calculo novamente aqui dentro.*/
+        double kWH = this.consumoPorDia;
+        return this.consumoEmReal;
     }
 
     //método que calcula o consumo por hora e devolve em Wh
